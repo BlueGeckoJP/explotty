c91fe59d1fac79765c1d915f0ac4c1196520346a
bluegecko <kei09doya@gmail.com>
2025-06-16 18:47:15 +0900

    feat: Enhance CSI sequence processing for terminal emulation

--- src/terminal_widget.rs
+++ src/terminal_widget.rs
@@ -217,7 +217,7 @@ impl TerminalWidget {
             }
         }
 
-        println!("CSI Sequence: {}", sequence);
+        //log::debug!("Processing CSI sequence: {}", sequence);
 
         // Process the CSI sequence
         match sequence.as_str() {
@@ -307,6 +307,182 @@ impl TerminalWidget {
                 todo!("Send response back to PTY: {response}");
                 // TODO: HERE
             }
+
+            // Erase in Display/Line - Erase in Display
+            ch if ch.ends_with('J') => {
+                let num = sequence.trim_end_matches('J').parse::<usize>().unwrap_or(0);
+                match num {
+                    0 => self.buffer.clear_screen(),
+                    // 1 => Erase entire screen (may include scroll buffer),
+                    // 2 => Erase the entire screen, including the scroll buffer (Linux 3.0 and later)
+                    _ => self.buffer.scroll_region_top = self.buffer.cursor_y,
+                }
+            }
+
+            // Erase in Display/Line - Erase in Line
+            ch if ch.ends_with('K') => {
+                let num = sequence.trim_end_matches('K').parse::<usize>().unwrap_or(0);
+                match num {
+                    0 => {
+                        // Erase from cursor to end of line
+                        for x in self.buffer.cursor_x..self.buffer.width {
+                            self.buffer.cells[self.buffer.cursor_y][x] = Default::default();
+                        }
+                    }
+                    1 => {
+                        // Erase from start of line to cursor
+                        for x in 0..=self.buffer.cursor_x {
+                            self.buffer.cells[self.buffer.cursor_y][x] = Default::default();
+                        }
+                    }
+                    2 => {
+                        // Erase entire line
+                        for x in 0..self.buffer.width {
+                            self.buffer.cells[self.buffer.cursor_y][x] = Default::default();
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            // Select Graphic Rendition (SGR)
+            ch if ch.ends_with('m') => {
+                let params: Vec<&str> = sequence.trim_end_matches('m').split(';').collect();
+                for param in params {
+                    match param {
+                        "0" => {
+                            // Reset all attributes
+                            self.buffer.current_fg_color = Color32::WHITE;
+                            self.buffer.current_bg_color = Color32::TRANSPARENT;
+                            self.buffer.current_bold = false;
+                            self.buffer.current_underline = false;
+                            self.buffer.current_italic = false;
+                        }
+                        "1" => self.buffer.current_bold = true,
+                        "2" => {
+                            let current_color = self.buffer.current_fg_color;
+                            self.buffer.current_fg_color = Color32::from_rgb(
+                                (current_color.r() as u16 * 4 / 5).min(255) as u8,
+                                (current_color.g() as u16 * 4 / 5).min(255) as u8,
+                                (current_color.b() as u16 * 4 / 5).min(255) as u8,
+                            );
+                        }
+                        "3" => self.buffer.current_italic = true,
+                        "4" => self.buffer.current_underline = true,
+                        // "5" => Blink
+                        // "7" => Reverse
+                        // "8" => Hidden
+                        // "9" => Strikethrough
+                        _ if param.contains("30") => {
+                            self.buffer.current_fg_color = Color32::BLACK;
+                        }
+                        _ if param.contains("31") => {
+                            self.buffer.current_fg_color = Color32::RED;
+                        }
+                        _ if param.contains("32") => {
+                            self.buffer.current_fg_color = Color32::GREEN;
+                        }
+                        _ if param.contains("33") => {
+                            self.buffer.current_fg_color = Color32::YELLOW;
+                        }
+                        _ if param.contains("34") => {
+                            self.buffer.current_fg_color = Color32::BLUE;
+                        }
+                        _ if param.contains("35") => {
+                            self.buffer.current_fg_color = Color32::MAGENTA;
+                        }
+                        _ if param.contains("36") => {
+                            self.buffer.current_fg_color = Color32::CYAN;
+                        }
+                        _ if param.contains("37") => {
+                            self.buffer.current_fg_color = Color32::WHITE;
+                        }
+                        _ if param.starts_with("38;5;") => {
+                            // 256-color foreground
+                            if let Ok(color_index) = param[5..].parse::<u8>() {
+                                self.buffer.current_fg_color =
+                                    Color32::from_rgb(color_index, color_index, color_index);
+                            }
+                        }
+                        _ if param.starts_with("38;2;") => {
+                            // 24bit RGB foreground
+                            let parts: Vec<&str> = param[5..].split(';').collect();
+                            if parts.len() == 3
+                                && let (Ok(r), Ok(g), Ok(b)) = (
+                                    parts[0].parse::<u8>(),
+                                    parts[1].parse::<u8>(),
+                                    parts[2].parse::<u8>(),
+                                )
+                            {
+                                self.buffer.current_fg_color = Color32::from_rgb(r, g, b);
+                            }
+                        }
+                        _ if param.starts_with("40") => {
+                            self.buffer.current_bg_color = Color32::BLACK;
+                        }
+                        _ if param.starts_with("41") => {
+                            self.buffer.current_bg_color = Color32::RED;
+                        }
+                        _ if param.starts_with("42") => {
+                            self.buffer.current_bg_color = Color32::GREEN;
+                        }
+                        _ if param.starts_with("43") => {
+                            self.buffer.current_bg_color = Color32::YELLOW;
+                        }
+                        _ if param.starts_with("44") => {
+                            self.buffer.current_bg_color = Color32::BLUE;
+                        }
+                        _ if param.starts_with("45") => {
+                            self.buffer.current_bg_color = Color32::MAGENTA;
+                        }
+                        _ if param.starts_with("46") => {
+                            self.buffer.current_bg_color = Color32::CYAN;
+                        }
+                        _ if param.starts_with("47") => {
+                            self.buffer.current_bg_color = Color32::WHITE;
+                        }
+                        _ if param.starts_with("48;5;") => {
+                            // 256-color background
+                            if let Ok(color_index) = param[5..].parse::<u8>() {
+                                self.buffer.current_bg_color =
+                                    Color32::from_rgb(color_index, color_index, color_index);
+                            }
+                        }
+                        _ if param.starts_with("48;2;") => {
+                            // 24bit RGB background
+                            let parts: Vec<&str> = param[5..].split(';').collect();
+                            if parts.len() == 3
+                                && let (Ok(r), Ok(g), Ok(b)) = (
+                                    parts[0].parse::<u8>(),
+                                    parts[1].parse::<u8>(),
+                                    parts[2].parse::<u8>(),
+                                )
+                            {
+                                self.buffer.current_bg_color = Color32::from_rgb(r, g, b);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            // Scroll Control - Scroll Up
+            // ch if ch.ends_with('S') => {}
+
+            // Scroll Control - Scroll Down
+            // ch if ch.ends_with('T') => {}
+
+            // Insert/delete lines/characters
+            // ch if ch.ends_with('L') => {} // Insert lines
+            // ch if ch.ends_with('M') => {} // Delete lines
+            // ch if ch.ends_with('P') => {} // Delete characters
+            // ch if ch.ends_with('X') => {} // Erase characters
+            // ch if ch.ends_with('@') => {} // Insert characters
+
+            // Set Mode/Reset Mode
+            // Not implemented yet
+
+            // Other CSI sequences
             _ => {}
         }
     }
